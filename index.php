0e239efd32d775bbba184b3cc5c0bf890b2f7c12
Mike Han <mike.han91@gmail.com>
2016-09-11 16:20:03 -0700

    Added Student 1 and 2 to index

--- index.php
+++ index.php
@@ -11,6 +11,40 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+            include('student.php');
+            
+            // Student object array
+            $students = array();
+            
+            // First student
+            $first = new Student();
+            $first -> surname = "Doe";
+            $first -> first_name = "John";
+            $first -> add_email('home', 'john@doe.com');
+            $first -> add_email('work', 'jdoe@mcdonalds.com');
+            $first -> add_grade(65);
+            $first -> add_grade(75);
+            $first -> add_grade(55);
+            
+            $students['j123'] = $first;
+            
+            // Second Student
+            $second = new Student();
+            $second -> surname = "Einstein";
+            $second -> first_name = "Albert";
+            $second ->add_email("home", 'albert@braniacs.com');
+            $second ->add_email('work1', 'a_einstein@bcit.ca');
+            $second ->add_email('work2', 'albert@physics.mit.edu');
+            $second ->add_grade(95);
+            $second ->add_grade(80);
+            $second ->add_grade(50);
+            
+            $Students['a456'] = $second;
+            
+            
+            foreach ($students as $student){
+                echo $student -> toString();
+            }
             echo "Hello out there ... I live";
         ?>
     </body>
